2059e89199a66de658fce535b3a6f0f03e648d37
Cnels1986 <CNels1986@gmail.com>
2017-11-17 14:51:22 -0500

    progress bar is complete, needs to be added to html

--- test/cnelstest.js
+++ test/cnelstest.js
@@ -15,3 +15,48 @@ var dayOfWeek = dayList[date.getDay()];
 var fullDate = dayOfWeek + ", " + monthName + " " + day + ", " + year;
 console.log(fullDate);
 document.getElementById("date").innerHTML = fullDate;
+
+
+// this code will fill the progress bar depending on the provided percentage
+var x = document.getElementById("progressBar");
+var width = 1;
+var id = setInterval(frame, 10);
+var percentage =50;
+function frame() {
+    if (width >= 100) {                 //clears if full
+        clearInterval(id);
+    } else if(width < percentage)       //fills bar until percentage
+    {
+        width++;
+        x.style.width = width + '%';
+    }
+}
+
+
+// // document.getElementById('nextButton').classList.add("greyOut");
+// //
+// // function checkChecked(box, button){
+// //   document.getElementById(button).disabled = !box.checked;
+// //   document.getElementById('nextButton').classList.remove("greyOut");
+// // }
+//
+// document.getElementById('nextButton').classList.add("greyOut");
+//
+// var checks = document.getElementsByClassName('test');
+// var checkBoxList = document.getElementById('checkBoxes');
+// var sendbtn = document.getElementById('nextButton');
+//
+// function allTrue(nodeList) {
+//   for (var i = 0; i < nodeList.length; i++) {
+//     if (nodeList[i].checked === false) return false;
+//   }
+//   return true;
+// }
+//
+// checkBoxList.addEventListener('click', function(event) {
+//   sendbtn.disabled = true;
+//   if (allTrue(checks)){
+//     sendbtn.disabled = false;
+//     document.getElementById('nextButton').classList.remove("greyOut");
+//   }
+// });
