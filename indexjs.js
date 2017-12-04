cbc94f2d3f001e5b74d3cfbd0b04e0ae2b9561e4
cggarcia171 <camerongarcia111@gmail.com>
2017-12-04 16:13:43 -0500

    finished the index for the most part

--- indexjs.js
+++ indexjs.js
@@ -24,15 +24,7 @@ function move(){
 }
 
 
-var bar = new ProgressBar.Line(indexProgress, {
-  strokeWidth: 3,
-  easing: 'easeInOut',
-  duration: 1400,
-  color: '#3bb9ba',
-  trailColor: '#4a4a4a',
-  trailWidth: 3,
-  svgStyle: {width: '100%', height: '100%'}
-});
+
 
 $(".loginSubmitButton").on("click", function(){
   var email = document.getElementById("loginEmail");
@@ -49,7 +41,7 @@ $(".loginSubmitButton").on("click", function(){
 
 })
 
-$("#indexSubmit").on("click", function(){  
+$("#indexSubmit").on("click", function(){
   var email = document.getElementById("emailAddress");
   if(email.value === '' || split(email) === false){
     $(".helpContent").effect("shake");
@@ -59,6 +51,62 @@ $("#indexSubmit").on("click", function(){
     progress = 33;
     $("#forgotPassword1").toggle();
     $("#forgotPassword2").toggle();
+    var bar = new ProgressBar.Line(indexProgress1, {
+      strokeWidth: 3,
+      easing: 'easeInOut',
+      duration: 1400,
+      color: '#3bb9ba',
+      trailColor: '#4a4a4a',
+      trailWidth: 3,
+      svgStyle: {width: '100%', height: '100%'}
+    });
     bar.animate(.33);
   }
+});
+$("#streetSubmit").on("click", function(){
+  var street = document.getElementById("streetQuestion");
+  if(street.value === ""){
+    $(".invalidMessage").text("wrong");
+    $('input').css("border", "1px solid #d0021b");
+    $('.helpContent').effect('shake');
+  }else if(street.value === "orange street"){
+    $("#forgotPassword2").toggle();
+    $("#forgotPassword3").toggle();
+    var bar = new ProgressBar.Line(indexProgress2, {
+      strokeWidth: 3,
+      easing: 'easeInOut',
+      duration: 1400,
+      color: '#3bb9ba',
+      trailColor: '#4a4a4a',
+      trailWidth: 3,
+      svgStyle: {width: '100%', height: '100%'}
+    });
+    bar.animate(.66);
+  }
+
+});
+$("#colorSubmit").on("click", function(){
+  var color = document.getElementById("colorQuestion");
+  if(color.value === ""){
+    $(".invalidMessage").text("wrong");
+    $('input').css("border", "1px solid #d0021b");
+    $('.helpContent').effect('shake');
+  }else if(color.value === "maroon"){
+    $("#forgotPassword3").toggle();
+    $("#forgotPassword4").toggle();
+    var bar = new ProgressBar.Line(indexProgress3, {
+      strokeWidth: 3,
+      easing: 'easeInOut',
+      duration: 1400,
+      color: '#3bb9ba',
+      trailColor: '#4a4a4a',
+      trailWidth: 3,
+      svgStyle: {width: '100%', height: '100%'}
+    });
+    bar.animate(1.0);
+  }
+
+});
+$("#finalBox").on("click", function(){
+  $("#forgotPassword4").toggle();
 });
